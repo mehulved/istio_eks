a1bab8aa4a31da712f03bc30c85e12e21ada310a
Mehul Ved <mehul.ved@nexsales.com>
2020-06-23 10:21:55 +0530

    Use non-overlapping IPs for VPC peering

--- terraform/sirius-b/vpc.tf
+++ terraform/sirius-b/vpc.tf
@@ -29,8 +29,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.names 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.110.0/24", "10.0.120.0/24", "10.0.130.0/24"]
 
   enable_nat_gateway   = true
   enable_vpn_gateway   = true
